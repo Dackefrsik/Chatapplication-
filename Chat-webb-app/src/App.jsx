89c31a1fee0921cfa997a5444d331bb3912558b7
Dackefrsik <danielfrisk21@gmail.com>
2025-09-22 17:05:20 +0200

    Node.js socket server
    Nu finns det en webbserver med Socket från Node.js där två enheter kan ansluta

--- Chat-webb-app/src/App.jsx
+++ Chat-webb-app/src/App.jsx
@@ -1,13 +1,35 @@
 import Navbar from './Components/Navbar';
 import Conversation  from './Components/Conversation';
 import TextInput from './Components/TextInput';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+//Importerar sockt för klienten
+import {io} from "socket.io-client";
 
 function App() {
 
   const[messages, addMessage] = useState([]);
 
   const[currentText, setCurrentText] = useState("");
+
+  const[socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+    console.log("Connected with id: " + newSocket.id)
+  })
+
+  return () => {
+    newSocket.disconnect();
+    console.log("Disconnected socket");
+  }
+
+  }, []);
+
 
   function returnMessage(inputMessage){
     console.log("Message from TextInput: ", inputMessage);
---
+++ Chat-webb-app/src/server/Server.js
@@ -0,0 +1,43 @@
+//importerar relevanta moduler
+import express from "express";
+import http from "http";
+import {Server} from "socket.io";
+
+//Skapar en express app, möjliggör definieringen av rputs (app.get/ app.post)
+const app = express();
+
+//Skapar https servern
+const server = http.createServer(app);
+
+//Sätter upp en socket.io server 
+const io = new Server(server, {
+    cors:{
+        origin: "*", // * Låter alla enheter på nätverket ansluta
+        methods: ["GET", "POST"],
+
+    },
+});
+
+//Regler för antalet som kan ansluta
+const clientMax = 2;
+let connectedClients = 0;
+
+//Sköter anslutningar till servern 
+io.on("connection", (socket) => {
+
+    //Kontrolerar antalet anslutna klienter
+    if(connectedClients >= clientMax){
+        console.log("Maximum clients reached. Denied connection: ", socket.id);
+        socket.disconnect(true);
+        return;
+    }
+    
+    connectedClients ++;
+    console.log("Klient anslöt", socket.id);
+});
+
+//Lysnar på servern
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000...");
+}); 
+
